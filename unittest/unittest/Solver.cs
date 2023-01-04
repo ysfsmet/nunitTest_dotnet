21df8e0c3307d97e254708d9c406d8e01a95ccd8
Yusuf Samet Demirci <ysf.samet@gmail.com>
2023-01-04 23:05:32 +0300

    added solver methods

--- unittest/unittest/Solver.cs
+++ unittest/unittest/Solver.cs
@@ -22,5 +22,56 @@
             }
             return result;
         }
+
+        public int SumOfDigits(int n)
+        {
+            var result = 0;
+            while (n > 0)
+            {
+                result += n % 10;
+                n /= 10;
+            }
+            return result;
+        }
+
+        public int AdjacentElementsProduct(IEnumerable<int> inputs)
+        {
+            if (inputs is null)
+                return 0;
+
+            var result = int.MinValue;
+            var lastItem = inputs.First();
+            for (int i = 1; i < inputs.Count(); i++)
+            {
+                var product = lastItem * inputs.ElementAt(i);
+                if (product > result) result = product;
+                lastItem = inputs.ElementAt(i);
+            }
+            return result;
+        }
+
+        public IEnumerable<int> AlternatingSums(IEnumerable<int> members)
+        {
+            var sumOfWeightsFirstGroup = 0;
+            var sumOfWeightsSecondGroup = 0;
+            for (int i = 0; i < members.Count(); i++)
+            {
+                if (i % 2 == 0) sumOfWeightsFirstGroup += members.ElementAt(i);
+                else sumOfWeightsSecondGroup += members.ElementAt(i);
+            }
+            return new int[] { sumOfWeightsFirstGroup, sumOfWeightsSecondGroup };
+        }
+
+        public IEnumerable<int> AlternatingSums(IEnumerable<int> members, int groupCount)
+        {
+            var result = new int[groupCount];
+            var groupIdx = 0;
+            for (int i = 0; i < members.Count(); i++)
+            {
+                result[groupIdx++] += members.ElementAt(i);
+                if (groupIdx == groupCount) groupIdx = 0;
+            }
+            return result;
+        }
     }
 }
--- unittest/unittestTest/SolverTest.cs
+++ unittest/unittestTest/SolverTest.cs
@@ -17,10 +17,42 @@ namespace unittestTest
         [TestCase(new int[] { 2, 4, 7, 6 }, ExpectedResult = 4)]
         [TestCase(new int[] { 2, 4, 7, 6, 6, 8 }, ExpectedResult = 6)]
         [TestCase(new int[] { 2, 4, 7, 6, 6 }, ExpectedResult = 6)]
-        [TestCase(null, ExpectedResult = 6)]
         public int FindSmallestValue_GivenSortedIntegerArray_ReturnSmallest(IEnumerable<int> array)
         {
             return solver.AbsoluteValuesSumMinimization(array);
+        }
+
+        [Test]
+        public void SumOfDigits_GivenInteger_ResultDigitsSum()
+        {
+            Assert.Multiple(() =>
+            {
+                Assert.That(solver.SumOfDigits(29), Is.EqualTo(11));
+                Assert.That(solver.SumOfDigits(90000), Is.EqualTo(9));
+                Assert.That(solver.SumOfDigits(111), Is.EqualTo(3));
+                Assert.That(solver.SumOfDigits(101010), Is.EqualTo(3));
+                Assert.That(solver.SumOfDigits(98765400), Is.EqualTo(39));
+            });
+        }
+
+        [Test]
+        public void AdjacentElementsProduct_GivenIntegerArray_ResultLargestProduct()
+        {
+            Assert.Multiple(() =>
+            {
+                Assert.That(solver.AdjacentElementsProduct(new int[] { 3, 6, -2, -5, 7, 3 }), Is.EqualTo(21));
+                Assert.That(solver.AdjacentElementsProduct(new int[] { 3, 6, -2, -5, -7, 3 }), Is.EqualTo(35));
+            });
+        }
+
+        [Test]
+        public void AlternatingSums_GivenIntegerArray_ResultGroupedSums()
+        {
+            Assert.Multiple(() =>
+            {
+                Assert.That(solver.AlternatingSums(new int[] { 50, 60, 60, 45, 70 }), Is.EqualTo(new int[] { 180, 105 }));
+                Assert.That(solver.AlternatingSums(new int[] { 15, 35, 30, 25, 10 }, 3), Is.EqualTo(new int[] { 40, 45, 30 }));
+            });
         }
     }
 }
